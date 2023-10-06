7910a8d7a261a41434aea4615a868d255a80c230
zsena <zsanett.tamas87@gmail.com>
2023-10-06 14:21:07 +0200

    Add project files.

---
+++ lev06/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.SetCursorPosition(20, 5);
+Console.Write("Ez az első programom!\n");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Szia Sisi!");
+Console.ResetColor();
+Console.WriteLine();
+Console.BackgroundColor = ConsoleColor.Green;
+Console.Write("\tMizu?");
+Console.ResetColor();
+Console.WriteLine("\nhogyvagy?");
+
+
+
+
+Console.ReadKey();
